923438971a4cb890eaca66871df1df55712ad7b4
iamchaze <virajkale9604@gmail.com>
2023-07-11 22:33:31 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+/*
+1. Use Inquirer package to get input from user.
+2. User qr-image package to convert user entered URL into QR image
+3. 3.Create a txt file to save the user input using native fs node module 
+*/
+
+import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from 'fs';
+
+inquirer
+  .prompt([
+    {
+      message: "Enter an URL: ",
+      name: "URL",
+    },
+  ])
+  .then((answers) => {
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream("qrimg.png"));
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
